c99243f4230d0ef536b82695e897b3a6e1c5264f
Olivia Steinmetz <livsteinmetz@Olivias-MacBook-Air.local>
2021-01-06 20:27:16 -0500

    final styling touches, mobile friendly

--- src/components/Liv/Liv.js
+++ src/components/Liv/Liv.js
@@ -6,13 +6,13 @@ export default function Liv() {
     <div id="container">
       <div id="left-container">
         <div id="left-scroll">
-          <h3>I’m working towards the development and adoption of ethical and inclusive technologies for sustainable social impact and innovation.</h3>
+          <h3>I’m working towards the development & adoption of ethical & inclusive technologies for sustainable social impact & innovation.</h3>
           <br></br>
-          <h5> My name is Olivia Steinmetz (she, her, hers), but you can
-              call me Liv. I'm a recent graduate of Vanderbilt with a dual degree in{' '}
-              <span className="color">Double Bass Performance</span> and{' '}
+          <h5> My name is <span className="color">Olivia Steinmetz (she/her)</span>, but you can
+              call me Liv. I'm a Philadelphia-based software engineer, musician, & UX designer. I received my Bachelors degree from Vanderbilt University with majors in {' '}
+              <span className="color">Double Bass Performance</span> &{' '}
               <span className="color">Engineering Science</span>. As an engineering
-              and music student, I focused on the intersection of{' '}
+              & music student, I focused on the intersection of{' '}
               <span className="color">technology, design, and social justice</span>.
               I’m committed to action and change, seeking out any and every
               opportunity to expand my knowledge in{' '}
@@ -30,16 +30,15 @@ export default function Liv() {
               I have completed research projects, coursework, and hackathons on user
               data research & best practices, blockchain for social impact,
               data-driven storytelling, the ethics of artificial intelligence,
-              sustainable urban development, and accessible design methodologies (to
-              name a few). My goal is to continue to be challenged and immersed in
-              work that is holistically human-focused, informed and inspired. I’m a
-              quick learner with a diverse background and am always up for a new
-              challenge. I thrive when I have the opportunity to wear several
-              different hats on fast-moving interdisciplinary teams.
+              sustainable urban development, & accessible design methodologies (to
+              name a few). My goal is to continue to be challenged & immersed in
+              work that is holistically human-focused, informed & inspired. I’m a
+              quick learner with a diverse background & am always up for a new
+              challenge.
             </p>
             <h6>
               You can find my resume{' '}
-              <a href="/resume.pdf" target="_blank" rel="noopener noreferrer">
+              <a href="/Olivia Steinmetz Resume.pdf" target="_blank" rel="noopener noreferrer">
                 <span className="color">here</span>
               </a>
               .
@@ -50,19 +49,31 @@ export default function Liv() {
             <section id="work">
               <Work />
             </section>
+
+            <br></br>
+            <br></br>
+
+            <h5 class="color">
+              Please reach out to learn more about my current projects! I'm always happy to chat :)
+			      </h5>
         </div>
       </div>
       <div id="right-container">
         <div id="right-scroll">
           <h1>Hi there!</h1>
-          <h2>My name is Liv and I’m a Philadelphia-based software engineer, musician, and UX designer.</h2>
+          <h2>My name is Liv & I’m a Philadelphia-based software engineer, musician, & UX designer.</h2>
           <h3>[Current Work]</h3>
           <h5>software engineer @ capital one</h5>
           <h5>freelance double bassist</h5>
           <h5>pro bono web developer</h5>
           <br></br>
           <h3>[Former Work + Clients + Collaborators]</h3>
-          <h5>Wond'ry Center for Innovation and Entrepreneurship, Allied Media Projects, Nashville Center for Nonprofit Management, Planned Parenthood, Project 615, Accenture Ops Studio, Vanderbilt University Medical Center, Children's Hospital of Philadelphia, Blair School of Music, Vanderbilt Programming Board (VPB) Speakers Committee, Students Consulting for Nonprofit Organizations (SCNO), Design for America, Design as an Immersive Vanderbilt Experience (DIVE) </h5>
+          <h5>Wond'ry Center for Innovation & Entrepreneurship, Allied Media Projects, Nashville Center for Nonprofit Management, Planned Parenthood, Project 615, Accenture Ops Studio, Vanderbilt University Medical Center, Children's Hospital of Philadelphia, Vanderbilt Programming Board (VPB) Speakers Committee, Students Consulting for Nonprofit Organizations (SCNO), Design for America, Design as an Immersive Vanderbilt Experience (DIVE) </h5>
+          <br></br>
+          <h3>[Contact]</h3>
+          <a href="https://www.linkedin.com/in/livsteinmetz/" target='_blank' rel='noopener noreferrer'><h5>LinkedIn</h5></a>
+          <a href="https://github.com/livsteinmetz" target='_blank' rel='noopener noreferrer'><h5>Github</h5></a>
+          <a href="mailto:olivia.r.steinmetz@gmail.com" target='_blank' rel='noopener noreferrer'><h5>Email</h5></a>
         </div>
 
       </div>
