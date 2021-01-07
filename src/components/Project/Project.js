8417252410b0ec92623da78c66ed011a2f903b68
Olivia Steinmetz <livsteinmetz@olivias-air.myfiosgateway.com>
2021-01-06 20:36:21 -0500

    add links

--- src/components/Project/Project.js
+++ src/components/Project/Project.js
@@ -3,9 +3,9 @@ import './Project.scss'
 export default function Project({ title, img, link, body }) {
 	return (
 		<div id="Project">
-			<a href={link} target="_blank" rel="noopener noreferrer">
+			{ link? <a href={link} target="_blank" rel="noopener noreferrer">
 				<h4>{title}</h4>			
-			</a>
+			</a> : <h4>{title}</h4>}
 			{ img ? <img src={img} alt={title} /> : null}
 			<p>{body}</p>
 		</div>
