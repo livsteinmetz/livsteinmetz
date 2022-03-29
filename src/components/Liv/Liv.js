8f5207d9c0b400560019ad6e178dc6761e40f064
livsteinmetz <olivia.r.steinmetz@gmail.com>
2022-03-28 22:55:21 -0400

    linting, add pointer on hover

--- src/components/Liv/Liv.js
+++ src/components/Liv/Liv.js
@@ -7,13 +7,13 @@ export default function Liv() {
     <div id="container">
       <div id="left-container">
         <div id="left-scroll">
-          <h3>I’m working towards the development & adoption of equitable & educational technologies for sustainable social impact & innovation.</h3>
+          <h3>I’m working towards the development and adoption of equitable and educational technologies for sustainable social impact and innovation.</h3>
           <br></br>
           <h5> My name is <span className="color">Olivia Steinmetz (she/her)</span>, but you can
-              call me Liv. I'm a Philadelphia-based software engineer, musician, & UI designer. I'm currently working at the<a className="color" href="https://barnesfoundation.org/" target='_blank' rel='noopener noreferrer'> Barnes Foundation</a> as a Front End Software Engineer. I received my B.S. from Vanderbilt University with majors in {' '}
-              <span className="color">Double Bass Performance</span> &{' '}
+              call me Liv. I'm a Philadelphia-based software engineer, musician, and UI designer. I'm currently working at the<a className="color" href="https://barnesfoundation.org/" target='_blank' rel='noopener noreferrer'> Barnes Foundation</a> as a Front End Software Engineer. I earned my Bachelor's degree from Vanderbilt University with majors in {' '}
+              <span className="color">Double Bass Performance</span> and{' '}
               <span className="color">Engineering Design</span>. As an engineering
-              & music student, I focused on the intersection of{' '}
+              and music student, I focused on the intersection of{' '}
               <span className="color">tech, design and culture.</span>.
               I’m committed to action and change, seeking out any and every
               opportunity to expand my knowledge in{' '}
@@ -29,12 +29,12 @@ export default function Liv() {
             </h5>
             <p>
            I have completed research projects, coursework, and hackathons on user
-              data research & best practices, blockchain for social impact,
+              data research and best practices, blockchain for social impact,
               data-driven storytelling, the ethics of artificial intelligence,
-              sustainable urban development, & accessible design methodologies (to
-              name a few). My goal is to continue to be challenged & immersed in
-              work that is holistically human-focused, informed & inspired. I’m a
-              quick learner with a diverse background & am always up for a new
+              sustainable urban development, and accessible design methodologies (to
+              name a few). My goal is to continue to be challenged and immersed in
+              work that is holistically human-focused, informed and inspired. I’m a
+              quick learner with a diverse background and am always up for a new
               challenge.
             </p>
             {/* <h6>
@@ -55,21 +55,21 @@ export default function Liv() {
             <br></br>
 
             <h5 class="color">
-              Interested in working together? Please reach out! I'm always happy to chat :)
+              Interested in working together? Want to learn more about my current projects? Please reach out! I'm always happy to chat :)
 			      </h5>
         </div>
       </div>
       <div id="right-container">
         <div id="right-scroll">
           <h1>Hi there!</h1>
-          <h2>My name is Liv & I’m a Philadelphia-based software engineer, musician, & UI designer.</h2>
+          <h2>My name is Liv and I’m a Philadelphia-based software engineer, musician, and UI designer.</h2>
           <h3>[Current Work]</h3>
           <h5>front end engineer @ <a href="https://barnesfoundation.org/" target='_blank' rel='noopener noreferrer'>the Barnes</a></h5>
           <h5>double bassist</h5>
           <h5>pro bono web developer</h5>
           <br></br>
           <h3>[Former Work + Clients + Collaborators]</h3>
-          <h5><a href="https://www.vanderbilt.edu/thewondry/" target='_blank' rel='noopener noreferrer'>The Wond'ry Center for Innovation & Entrepreneurship</a>, <a href="https://studios.accenture.com/" target='_blank' rel='noopener noreferrer'>Accenture Operations Studio</a>, <a href="https://alliedmedia.org/" target='_blank' rel='noopener noreferrer'>Allied Media Projects</a>, <a href="https://designjustice.org/" target='_blank' rel='noopener noreferrer'>Design Justice Network</a>, <a href="https://www.vanderbilt.edu/dive/" target='_blank' rel='noopener noreferrer'>Design as an Immersive Vanderbilt Experience (DIVE)</a>, <a href="https://designforamerica.com/" target='_blank' rel='noopener noreferrer'>Design for America</a>, Planned Parenthood, Vanderbilt University Medical Center, The Children's Hospital of Philadelphia, Vanderbilt Programming Board (VPB) Speakers Committee, <a href="https://www.vanderbiltscno.com/" target='_blank' rel='noopener noreferrer'>Students Consulting for Nonprofit Organizations (SCNO)</a>, <a href="https://www.cnm.org/" target='_blank' rel='noopener noreferrer'>Nashville Center for Nonprofit Management</a></h5>
+          <h5><a href="https://www.vanderbilt.edu/thewondry/" target='_blank' rel='noopener noreferrer'>The Wond'ry Center for Innovation and Entrepreneurship</a>, <a href="https://studios.accenture.com/" target='_blank' rel='noopener noreferrer'>Accenture Operations Studio</a>, <a href="https://alliedmedia.org/" target='_blank' rel='noopener noreferrer'>Allied Media Projects</a>, <a href="https://designjustice.org/" target='_blank' rel='noopener noreferrer'>Design Justice Network</a>, <a href="https://www.vanderbilt.edu/dive/" target='_blank' rel='noopener noreferrer'>Design as an Immersive Vanderbilt Experience (DIVE)</a>, <a href="https://designforamerica.com/" target='_blank' rel='noopener noreferrer'>Design for America</a>, Planned Parenthood, Vanderbilt University Medical Center, The Children's Hospital of Philadelphia, Vanderbilt Programming Board (VPB) Speakers Committee, <a href="https://www.vanderbiltscno.com/" target='_blank' rel='noopener noreferrer'>Students Consulting for Nonprofit Organizations (SCNO)</a>, <a href="https://www.cnm.org/" target='_blank' rel='noopener noreferrer'>Nashville Center for Nonprofit Management</a></h5>
           <br></br>
           <h3>[Contact]</h3>
           <a href="https://www.linkedin.com/in/livsteinmetz/" target='_blank' rel='noopener noreferrer'><h5>LinkedIn</h5></a>
