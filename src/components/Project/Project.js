4a579e5b459dbb156a9d721eeb6c2edfbb1d7d72
livsteinmetz <olivia.r.steinmetz@gmail.com>
2023-05-24 19:34:44 -0400

    update projects

--- src/components/Project/Project.js
+++ src/components/Project/Project.js
@@ -1,13 +1,18 @@
-import './Project.scss'
+import "./Project.scss";
 
-export default function Project({ title, img, link, body }) {
-	return (
-		<div id="Project">
-			{ link? <a href={link} target="_blank" rel="noopener noreferrer">
-				<h4>{title}</h4>			
-			</a> : <h4>{title}</h4>}
-			{ img ? <img src={img} alt={title} /> : null}
-			<p>{body}</p>
-		</div>
-	)
+export default function Project({ title, img, img2, link, body }) {
+  return (
+    <div id="Project">
+      {link ? (
+        <a href={link} target="_blank" rel="noopener noreferrer">
+          <h4>{title}</h4>
+        </a>
+      ) : (
+        <h4>{title}</h4>
+      )}
+      {img ? <img src={img} alt={title} /> : null}
+      {img2 ? <img src={img2} alt={title} /> : null}
+      <p>{body}</p>
+    </div>
+  );
 }
--- src/components/Work/Work.js
+++ src/components/Work/Work.js
@@ -1,21 +1,22 @@
-import React from 'react'
-import './Work.scss'
-import Project from '../Project/Project'
-import projects from '../../copy/projects.json'
+import React from "react";
+import "./Work.scss";
+import Project from "../Project/Project";
+import projects from "../../copy/projects.json";
 
 export default function Work() {
-	return (
-		<div id="Work">
-			<h3 id="workTop">Projects & Research</h3>
-			{projects.map(p => (
-				<Project
-					key={p.title}
-					title={p.title}
-					img={p.img}
-					link={p.link}
-					body={p.body}
-				/>
-			))}
-		</div>
-	)
+  return (
+    <div id="Work">
+      <h3 id="workTop">Projects & Research</h3>
+      {projects.map((p) => (
+        <Project
+          key={p.title}
+          title={p.title}
+          img={p.img}
+          img2={p.img2}
+          link={p.link}
+          body={p.body}
+        />
+      ))}
+    </div>
+  );
 }
